19ec75a4de45f22f67d8a1d1a39ca033e0a5fd8d
Harsha Ivaturi <harsha.ivaturi@gmail.com>
2022-04-02 23:49:06 +0530

    Add typography basics

--- src/packages/tokens/typography.css.ts
+++ src/packages/tokens/typography.css.ts
@@ -1,4 +1,7 @@
+import { defineProperties } from "@vanilla-extract/sprinkles";
+
 import { ScaleObject } from "./types";
+import { scaleToRem } from "./utils";
 
 const fontSize: ScaleObject = {
   s: 0.75,
@@ -6,3 +9,16 @@ const fontSize: ScaleObject = {
   l: 2,
   xl: 2.5,
 };
+
+const fontFamily = {
+  body: '-apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Helvetica, Arial, sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol"',
+};
+
+const typographyProperties = defineProperties({
+  properties: {
+    fontSize: scaleToRem(fontSize),
+    fontFamily,
+  },
+});
+
+export default typographyProperties;
