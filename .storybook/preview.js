9758031cc4d87a2574b7c9369c576b5494533576
Harsha Ivaturi <harsha.ivaturi@gmail.com>
2022-04-10 21:58:55 +0530

    Add storybook-dark-mode and setup dark mode

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,3 +1,20 @@
+import React, { useEffect } from "react";
+import { useDarkMode } from "storybook-dark-mode";
+
+export const decorators = [
+  (Story) => {
+    const isDarkMode = useDarkMode();
+    useEffect(
+      () =>
+        (document.documentElement.dataset.theme = isDarkMode
+          ? "dark"
+          : "light"),
+      [isDarkMode]
+    );
+    return Story();
+  },
+];
+
 export const parameters = {
   actions: { argTypesRegex: "^on[A-Z].*" },
   controls: {
@@ -6,4 +23,4 @@ export const parameters = {
       date: /Date$/,
     },
   },
-}
+};
